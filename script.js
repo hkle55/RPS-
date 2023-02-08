63bce7853c623b8552dc025a414abd1b0384e981
Kevin <108165494+hkle55@users.noreply.github.com>
2023-02-08 17:27:56 -0600

    make it so playerchoice is always lowercase no matter how they capitalize their choice

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function getComputerChoice(a= 3) {
     let choice = Math.floor(Math.random() * a + 1)
     if(choice == 1) { 
-        return "Rock"
+        return "rock"
     } else if(choice == 2) {
-        return "Paper"
+        return "paper"
     } else if(choice == 3) {
-        return "Scissors"
+        return "scissors"
     }
     }
 
 function playRound(playerChoice, computerChoice) {
-    if(playerChoice == "Rock" && computerChoice == "Paper") {
-        return "Computer wins"
+    if(playerChoice === computerChoice) {
+        return alert(`It's a tie! you both chose ${computerChoice}`)
     }
-}
+
+    }
+
 let playerChioce = prompt("Rock, Paper Or scissors?")
+let playerChoice = playerChioce.toLowerCase();
 let computerChoice = getComputerChoice()
-console.log(playRound(playerChioce,computerChoice))
+console.log(playRound(playerChoice,computerChoice))
 console.log(playerChioce)
+console.log(playerChoice)
 console.log(computerChoice)
